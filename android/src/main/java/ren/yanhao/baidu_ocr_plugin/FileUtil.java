bba371dc89d1464b63402afc3562868517fa15b8
严浩 <me@yanhao.ren>
2022-12-22 00:14:35 +0800

    FileUtil.saveFile

--- android/src/main/java/ren/yanhao/baidu_ocr_plugin/FileUtil.java
+++ android/src/main/java/ren/yanhao/baidu_ocr_plugin/FileUtil.java
@@ -6,9 +6,24 @@ package ren.yanhao.baidu_ocr_plugin;
 import android.content.Context;
 
 import java.io.File;
+import java.io.FileOutputStream;
 
 public class FileUtil {
-  public static File getSaveFile(Context context) {
-    return new File(context.getFilesDir(), "recognize.jpg");
-  }
+    public static File getSaveFile(Context context) {
+        return new File(context.getFilesDir(), "recognize.jpg");
+    }
+
+    //  将byte[]保存
+    @SuppressWarnings("ResultOfMethodCallIgnored")
+    public static void saveFile(Context context, byte[] data) {
+        File file = getSaveFile(context);
+        try {
+            file.createNewFile();
+            FileOutputStream out = new FileOutputStream(file);
+            out.write(data);
+            out.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
